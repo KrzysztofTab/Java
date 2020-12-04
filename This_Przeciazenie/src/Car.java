03899254d5afce6ee851edc1e897f2e6f9e854ae
Krzysztof <61905322+KrzysztofTab@users.noreply.github.com>
2020-12-04 20:19:42 +0100

    Create README.md

---
+++ This_Przeciazenie/src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    int year;
+    String brand;
+    String model;
+    String color;
+
+    Car(int yearA, String brandA, String modelA, String colorA) {
+        year = yearA;
+        brand = brandA;
+        model = modelA;
+        color = colorA;
+    }
+    Car(int yearB, String brandB, String modelB){
+        year = yearB;
+        brand = brandB;
+        model = modelB;
+}
+
+}
