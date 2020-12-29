591baf087442334a4436b66eb9b211cf65e9d732
Krzysztof <61905322+KrzysztofTab@users.noreply.github.com>
2020-12-29 11:35:17 +0100

    Merge remote-tracking branch 'origin/main' into main

--- Projekty/src/Projekt01/Libary/app/Library.java
+++ Projekty/src/Projekt01/Libary/app/Library.java
@@ -7,7 +7,7 @@ public class Library {
         final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
-                296, "Greg,", "9788373271890");
+                296, "Greg", "9788373271890");
         Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
                 2009, 352, "Helion", "9788324620845");
         Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
