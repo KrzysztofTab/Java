015df6d229c0643ad8ee02cdd9390f2cbe3869ce
Krzysztof <61905322+KrzysztofTab@users.noreply.github.com>
2021-01-05 23:37:56 +0100

    Merge remote-tracking branch 'origin/main' into main

--- KonstrukcjaSuper/src/GroceryShop/GroceryShop.java
+++
@@ -1,8 +0,0 @@
-package GroceryShop;
-
-public class GroceryShop {
-    public static void main(String[] args) {
-        Apple apple = new Apple(220, "szampion");
-        System.out.println(apple.getinfo());
-    }
-}
